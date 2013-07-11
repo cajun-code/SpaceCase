5f894efd7931e74020f4750e806f3e710ad5f896
Allan Davis <cajun.code@gmail.com>
2013-07-11 08:52:45 -0400

    Initial Commit

---
+++ SpaceCase/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpaceCaseAppController.h
+//  SpaceCase
+//
+//  Created by Allan Davis on 7/11/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
